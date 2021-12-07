f7a153f5b344d522491b3728d88bfb9b89e6965a
Görcsös Zoltán <gorcsos.zoltan@gmail.com>
2021-12-07 09:03:52 +0100

    Fix test project name in comment

--- src/backend/UnitTestAssemblyBeFriend.cs
+++ src/backend/UnitTestAssemblyBeFriend.cs
@@ -1,2 +1,2 @@
 ﻿using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Log4Pro.CoreComponents.Test")] // Log4Pro.CoreComponents.CoreComponents.Test project now can see the internals
+[assembly: InternalsVisibleTo("Log4Pro.CoreComponents.Test")] // Log4Pro.CoreComponents.Test project now can see the internals
