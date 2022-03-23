07e1796fd935e092a2dc9982d8fcbc934147f5e8
Görcsös Zoltán <gorcsos.zoltan@gmail.com>
2022-03-23 16:04:49 +0100

    v1.0.1: There is fixes in OperationMessageCenter part: - Extends the MessageCategory enum with Title and Description attribute, so you can use directly this enum in VSetting defination classes.

--- src/backend/OperationMessageCenter/MessageCategory.cs
+++ src/backend/OperationMessageCenter/MessageCategory.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Log4Pro.CoreComponents.Settings;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
@@ -17,22 +18,32 @@ namespace Log4Pro.CoreComponents.OperationMessageCenter
 		/// <summary>
 		/// Fatális hiba 
 		/// </summary>
+		[Title("Végzetes hibák")]
+		[Description("Az alapvető működést megakadályozó hibák kerülnek csak feljegyzésre.")]
 		Fatal,
 		/// <summary>
 		/// Hiba
 		/// </summary>
+		[Title("Hibák")]
+		[Description("Minden keletkező hiba feljegyzésre kerül.")]
 		Error,
 		/// <summary>
 		/// Figyelmeztetés
 		/// </summary>
+		[Title("Figyelmeztetések")]
+		[Description("Minden hiba és figyelmeztetés feljegyzésre kerül.")]
 		Warning,
 		/// <summary>
 		/// Informális üzenet a működésről
 		/// </summary>
+		[Title("Működéi információk")]
+		[Description("Minden fő működési információ, hiba és figyelmeztetés feljegyzésre kerül.")]
 		Information,
 		/// <summary>
 		/// Részletes működési információ
 		/// </summary>
+		[Title("Részletes információk")]
+		[Description("Minden rendelkezésre álló információ, hiba és figyelmeztetés feljegyzésre kerül.")]
 		DetailInformation,
 	}
 }
